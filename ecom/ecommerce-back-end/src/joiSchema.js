0fce268b7994924f9227a7e2bb9937c9741215c5
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2023-02-04 05:55:24 -0800

    Delete ecom/ecommerce-back-end directory

--- ecom/ecommerce-back-end/src/joiSchema.js
+++
@@ -1,9 +0,0 @@
-const Joi = require("joi");
-
-module.exports.userSchema = Joi.object({
-    firstName: Joi.string().required(),
-    lastName: Joi.string().required(),
-    userName: Joi.string().required(),
-    email: Joi.string().email().required(),
-    password: Joi.required()
-})
