5d0ba6b9298a3215722a9fd38c3d4fb64bea855f
BrainCellCoder <00abhisheksharma1999@gmail.com>
2023-02-25 10:11:22 +0530

    feat: added response to invalid api calls

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-require('dotenv').config({path: "./.env"})
-const app = require('./index');
+require("dotenv").config({ path: "./.env" });
+const app = require("./index");
 
 const DB = process.env.DB_URL;
-mongoose.connect(DB,{
+mongoose
+  .connect(DB, {
     useNewUrlParser: true,
-    useUnifiedTopology:true
-}).then(con => {
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
     console.log("DB connected successfully");
-}).catch(e => {
+  })
+  .catch((e) => {
     console.log(e);
-})
+  });
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
