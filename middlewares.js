b1b7ea15ba3e9f4f8b34f855100abbc2be88cd86
BrainCellCoder <00abhisheksharma1999@gmail.com>
2023-03-13 14:06:05 +0530

    fix: jwt bug

--- middlewares.js
+++ middlewares.js
@@ -10,7 +10,7 @@ exports.isLoggedIn = async (req, res, next) => {
   //     })
   // }
   const { token } = req.cookies;
-  if (token === "j:null") {
+  if (token === "j:null" || token === undefined) {
     return res.status(400).json({
       success: false,
       message: "You are not Logged In",
