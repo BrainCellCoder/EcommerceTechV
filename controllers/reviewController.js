94b225568c495d41c4fd7ecb28d6eaa6f6f1c91b
BrainCellCoder <00abhisheksharma1999@gmail.com>
2023-04-05 13:15:16 +0530

    feat: avg rating of a product

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -4,11 +4,23 @@ const Review = require("../models/reviewModel");
 exports.addReview = async (req, res) => {
   try {
     const { id } = req.params;
-    const product = await Product.findById(id);
+    const product = await Product.findById(id).populate({
+      path: "reviews",
+      populate: {
+        path: "author",
+        model: "User",
+      },
+    });
     const review = new Review(req.body);
     review.author = req.userData.id;
     product.reviews.push(review);
+
     product.numOfReviews = product.reviews.length;
+
+    let totalNumReviews = 0;
+    Number(product.reviews.forEach((rev) => (totalNumReviews += rev.rating)));
+    product.rating = Math.round(totalNumReviews / product.reviews.length);
+
     await product.save();
     await review.save();
     return res.json({
