13f91ff6c4ebef8b4d0b2dcbf1d105fad213ba14
BrainCellCoder <00abhisheksharma1999@gmail.com>
2023-05-13 17:44:10 +0530

    cleanup

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -20,7 +20,6 @@ exports.checkout = async (req, res) => {
     const products = cart.map((c) => {
       return { productId: c.productId, quantity: c.quantity };
     });
-    await Order.deleteMany({});
     const newOrder = await Order.create({
       products,
       buyer,
